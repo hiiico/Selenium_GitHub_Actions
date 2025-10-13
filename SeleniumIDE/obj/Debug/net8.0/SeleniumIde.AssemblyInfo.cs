226cc3bf597a9bea58c3329121ec551da027fdb5
hiiico <hiiico332@gmail.com>
2025-10-13 18:04:45 +0300

    set

--- SeleniumIDE/obj/Debug/net8.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net8.0/SeleniumIde.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058911e5f45e2ad456fe9d29de1bb406730b5b20")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
